da65bf1aea60968f49f5302a982aa64e74f577bf
GeekHubDeveloper <47343311+GeekHubDeveloper@users.noreply.github.com>
2019-02-10 15:11:45 +0900

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,6 +3,6 @@ public class Test {
         System.out.println("simple:cat");
         System.out.println("plugin-tester1");
         System.out.println("plugin-tester2");
-        System.out.println("plugin-tester3");
+        System.out.println("GeekHub-Tester");
     }
 }
