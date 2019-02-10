93cddf8bbd3ed9be1954729b07f4098021f1e484
GeekHubDeveloper <47343311+GeekHubDeveloper@users.noreply.github.com>
2019-02-10 14:52:28 +0900

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("simple:cat");
+        System.out.println("plugin-tester1");
+        System.out.println("plugin-tester2");
+        System.out.println("plugin-tester3");
+    }
+}
